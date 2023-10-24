ed1df9e272930f3d57efd1594fac804a1fd0fee1
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2023-10-24 01:54:05 +0000

    Added Diaganol Movement

--- CONFIG.java
+++ CONFIG.java
@@ -1,15 +1,21 @@
 // This file contains all configuration stuff
 class CONFIG {
     class CONTROLLER {
-        final double DEADZONE = 0.05;
+        // Distance from the center of the joystick before the action is considered a movement 
+        final float PROCCESS_DEADZONE = 0.75;
+
+        // How far a joystick has to move before anything is detected
+        final float DEADZONE = 0.10;
     }
     class DRIVETRAIN {
-        // A value between 0 and 1 to which motor power will be scaled
-        final double FOWARD_SPEED = 1.00;
-        final double TURN_SPEED = 1.00;
+        // A value between 0 and 1 which the motor power will be set to
+        final float FOWARD_SPEED = 1.00;
+        final float TURN_SPEED = 1.00;
 
         // String Values which correspond to the DriverHub's hardware map
-        final String RIGHT = "RightMotor";
-        final String LEFT = "LeftMotor";
+        final String FRONT_RIGHT = "FRMotor";
+        final String FRONT_LEFT = "FLMotor";
+        final String BACK_RIGHT = "FRMotor";
+        final String BACK_LEFT = "FLMotor";
     }
 }
--- DrivetrainControl.java
+++ DrivetrainControl.java
@@ -11,7 +11,14 @@ public class DrivetrainControl extends OpMode{
     DcMotor mtFR = null;
     DcMotor mtFL = null;
     double deadzone = 0.05;
-    
+
+    // Positions of joysticks
+    float leftX = 0;
+    float leftY = 0;
+
+    float rightX = 0;
+    float rightY = 0;
+
     public void init() {
     // Setup motors
         // Motors are first identified with the 'mt' (Motor)
@@ -19,28 +26,73 @@ public class DrivetrainControl extends OpMode{
         // Next the side is identified with L (Left) or R (Right) 
         
         // Map the motors
-        mtFR = hardwareMap.get(DcMotor.class, CONFIG.DRIVETRAIN.RIGHT);
-        mtFL = hardwareMap.get(DcMotor.class, CONFIG.DRIVETRAIN.RIGHT);
+        mtFR = hardwareMap.get(DcMotor.class, CONFIG.DRIVETRAIN.FRONT_RIGHT);
+        mtFL = hardwareMap.get(DcMotor.class, CONFIG.DRIVETRAIN.FRONT_LEFT);
+        mtBR = hardwareMap.get(DcMotor.class, CONFIG.DRIVETRAIN.BACK_RIGHT);
+        mtBL = hardwareMap.get(DcMotor.class, CONFIG.DRIVETRAIN.BACK_LEFT);
 
         // What to do if there's no power being provided
-        mtFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        mtFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // mtFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // mtFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // mtFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // mtFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        mtFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        mtFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // mtFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // mtFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // mtFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // mtFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     
-        mtFR.setDirection(DcMotorSimple.Direction.FORWARD);
-        mtFL.setDirection(DcMotorSimple.Direction.FORWARD);
+        // mtFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        // mtFL.setDirection(DcMotorSimple.Direction.FORWARD);
 
     // Gamepad setup
         gamepad1 = new GamePad();
         gamepad1.setJoyStickDeadzone(CONFIG.CONTROLLER.DEADZONE);
     }
     
+
     public void loop() {
+        // If the gamepad has input from a joystick then run
         if( !gamepad1.atRest() ) {
-            mtFR.setPower(-gamepad1.left_stick_y * THROTTLE);
-            mtFL.setPower(gamepad1.left_stick_y * THROTTLE);
+            leftX = gamepad1.left_stick_x;
+            leftY = gamepad1.left_stick_y;
+            rightX = gamepad1.right_stick_x;
+            rightY = gamepad1.right_stick_y;
+
+            float dz = CONFIG.CONTROLLER.PROCCESS_DEADZONE;
+            float th = CONFIG.DRIVETRAIN.THROTTLE;
+
+            // Array to store what each wheel will do at the end of the end of the cycle
+            // In this order FL, FR, BL, BR
+            float[] wheelMoves = {0, 0, 
+                                  0, 0};
+
+            // Diagonal Movement
+            if (-dz > leftX && dz < leftY) {
+                // D FL
+                wheelMoves = {0, 1, 
+                              1, 0};
+            }
+            if (dz < leftX && -dz > leftY) {
+                // D BL
+                wheelMoves = {0, -1, 
+                              -1, 0};
+            }
+            if (dz < leftX && dz < leftY) {
+                // D FR
+                wheelMoves = {1, 0, 
+                              0, 1};
+            }
+            if (-dz > leftX && -dz > leftY) {
+                // D BL
+                wheelMoves = {-1, 0, 
+                              0, -1};
+            }
+
+            mtFL.setPower(wheelMoves[0] * th);
+            mtFR.setPower(wheelMoves[1] * th);
+            mtBL.setPower(wheelMoves[2] * th);
+            mtBR.setPower(wheelMoves[3] * th);
         }
     }
 
