77a2dcc668d56e8e1561f664a165bf932e85d646
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2023-10-25 04:17:39 +0000

    Cleaned up + Added advanced control mode

--- CONFIG.java
+++ CONFIG.java
@@ -1,16 +1,15 @@
 // This file contains all configuration stuff
 class CONFIG {
     class CONTROLLER {
-        // Distance from the center of the joystick before the action is considered a movement 
-        final float PROCCESS_DEADZONE = 0.75;
-
         // How far a joystick has to move before anything is detected
-        final float DEADZONE = 0.10;
+        final float DEADZONE = 0.60;
+
+        // More advanced movement + combos, not recommended for competition
+        boolean ADVANCED_CONTROL = false;
     }
     class DRIVETRAIN {
         // A value between 0 and 1 which the motor power will be set to
-        final float FOWARD_SPEED = 1.00;
-        final float TURN_SPEED = 1.00;
+        final float SPEED = 1.00;
 
         // String Values which correspond to the DriverHub's hardware map
         final String FRONT_RIGHT = "FRMotor";
