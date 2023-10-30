e942f94cb4d50e73473d41218c2c6f415846b0e1
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2023-10-30 23:17:04 +0000

    Fixed up some access specifiers and keywords

--- club/LibertyRobotics/CONFIG.java
+++ club/LibertyRobotics/CONFIG.java
@@ -1,8 +1,8 @@
 package club.LibertyRobotics;
 
 // This file contains all configuration stuff
-static class CONFIG {
-    static class CONTROLLER {
+final class CONFIG {
+    final class CONTROLLER {
         // How far a joystick has to move before anything is detected
         final static float DEADZONE = 0.60;
 
@@ -10,7 +10,7 @@ static class CONFIG {
         // Tries to blend multiple moves together for quicker movement
         final static boolean SMOOTH_DRIVING = false;
     }
-    static class DRIVETRAIN {
+    final class DRIVETRAIN {
         // A value between 0 and 1 which the motor power will be set to
         final static float SPEED = 0.25;
 
