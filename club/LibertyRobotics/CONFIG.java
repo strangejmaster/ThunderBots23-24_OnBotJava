955f96117900093b94d0e1d0b3c88da8472613cf
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2023-10-31 01:30:35 +0000

    Added public access specifiers

--- club/LibertyRobotics/CONFIG.java
+++ club/LibertyRobotics/CONFIG.java
@@ -1,26 +1,29 @@
 package club.LibertyRobotics;
 
 // This file contains all configuration stuff
-final class CONFIG {
-    final class CONTROLLER {
+public public final class CONFIG {
+    public public final class CONTROLLER {
         // How far a joystick has to move before anything is detected
-        final static float STICK_DEADZONE = 0.60f;
+        public final static float STICK_DEADZONE = 0.60f;
         
         // Minimum deadzone before the triggers activate
-        final static float TRIGGER_DEADZONE = 0.40f;
+        public final static float TRIGGER_DEADZONE = 0.40f;
 
         // **EXPERIMENTAL**
         // Tries to blend multiple moves together for quicker movement
-        final static boolean SMOOTH_DRIVING = false;
+        public final static boolean SMOOTH_DRIVING = false;
     }
-    final class DRIVETRAIN {
+    public public final class DRIVETRAIN {
         // A value between 0 and 1 which the motor power will be set to
-        final static float SPEED = 0.25f;
+        public final static float SPEED = 0.25f;
+
+        // Radius of the drive train wheels in mm
+        public final static float WHEEL_RADIUS = 6.0f;
 
         // String Values which correspond to the DriverHub's hardware map
-        final static String FRONT_RIGHT = "FRMotor";
-        final static String FRONT_LEFT = "FLMotor";
-        final static String BACK_RIGHT = "FRMotor";
-        final static String BACK_LEFT = "FLMotor";
+        public final static String FRONT_RIGHT = "FRMotor";
+        public final static String FRONT_LEFT = "FLMotor";
+        public final static String BACK_RIGHT = "FRMotor";
+        public final static String BACK_LEFT = "FLMotor";
     }
 }
